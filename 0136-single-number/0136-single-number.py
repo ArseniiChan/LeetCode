9792bf807f9da290edb622db4d6338def6b14a98
Arsenii Chan <arseniichan9@gmail.com>
2025-12-05 11:57:03 -0500

    Time: 7 ms (32.82%), Space: 19.7 MB (43.1%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = {}
+        for n in nums:
+            if n in freq:
+                freq[n] += 1
+            else:
+                freq[n] = 1
+        for n, count in freq.items():
+            if count == 1:
+                return n    
+            
+
+        
